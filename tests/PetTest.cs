cbb2a180d3c2058fc21451de20368c6aa62316e1
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-10-17 16:57:20 -0300

    Criação do codigo de testes

--- tests/PetTest.cs
+++ tests/PetTest.cs
@@ -1,5 +1,6 @@
 // 1 - Bibliotecas
 using RestSharp;
+using Newtonsoft.Json; // Dependencia para o JsonConvert
 // 2 - NameSpace
 public class Pet;
 // 3 - Classe
@@ -22,11 +23,31 @@ public class PetTest
         var request = new RestRequest("pet", Method.Post);
 
         // Armazena o conteudo do arquivo pet.json na memoria
-        string jsonBody = File.ReadAllText(@"D:\ESTUDO\ITERASYS\FTS139\petstore139\fixtures\pet1.json")
-
+        String jsonBody = File.ReadAllText(@"D:\ESTUDO\ITERASYS\FTS139\petstore139\fixtures\pet1.json")
+        // Adiciona na requisição o conteudo do arquivo pet1.json
+        request.AddBody(jsonBody);
         // Executa
-        
+        // Executa a requisição conforme a configuração realizada
+        // Guarda o json retornado no objeto response
+        var response = client.Execute(request);
 
         // Valida
+        var responseBody = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        // Exibe o responseBody no console
+        Console.WriteLine(responseBody);
+
+        // Valide que na resposta o status code é igual ao resultado esperado (200)
+        Assert.That((int)response.StatusCode, Is.EqualTo(200));
+
+        // Valida o nome do animal na resposta
+        String name = responseBody.name.ToString();
+        Assert.That(name, Is.EqualTo("Lagertha"));
+        // OU
+        // Assert.That(responseBody.name.ToString(), Is.EqualTo("Lagertha"));
+
+        // Valida o status do animal na resposta
+        string status = responseBody.status;
+        Assert.That(status, Is.EqualTo("available")); 
     }
 }    
