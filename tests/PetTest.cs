372710eda977a603b1982091384edfba03d30bc7
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-10-23 22:38:00 -0300

    implementação_de_codigo_de_teste

--- tests/PetTest.cs
+++ tests/PetTest.cs
@@ -11,7 +11,29 @@ public class PetTest
     // Endereço da API
     private const string BASE_URL = "https://petstore.swagger.io/v2/";
 
+
     // 3.2 - Funções e Métodos
+
+    // Função de leitura de dados a partir de um arquivo CSV
+    public static IEnumerable<TestCaseData> getTestData()
+    {
+        String caminhoMassa = @"D:\ESTUDO\ITERASYS\FTS139\petstore139\fixtures\pets.csv";
+
+        using var reader = new StreamReader(caminhoMassa);
+
+        // Pula a primeira linha com os cabeçalhos
+        reader.ReadLine();
+
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var values = line.Split(", ");
+
+            yield return new TestCaseData(int.Parse(values[0]), int.Parse(values[1]), values[2], values[3], values[4], values[5], values[6], values[7]);
+        }
+
+    }
+
     [Test, Order(1)]
     public void PostPetTest()
     {
@@ -145,5 +167,73 @@ public class PetTest
         
 
     }    
+    [TestCaseSource("getTestData", new object[]{}), Order(5)]
+    public void PostPetDDTTest(int petId,
+                                int categoryId,
+                                String categoryName,
+                                String petName,
+                                String photoUrls,
+                                String tagsIds,
+                                String tagsName,
+                                String status
+                                )
+    {
+        // Configura
+
+        PetModel petModel = new PetModel();
+        petModel.id = petId;
+        petModel.category = new Category(categoryId, categoryName);
+        petModel.name = petName;
+        petModel.photoUrls = new String[]{photoUrls};
+        petModel.tags = new Tag[]{new Tag(1, "vacinado"), 
+                                  new Tag(2, "castrado")};
+        petModel.status = status;
+
+
+        // Instancia o objeto do tipo RestClient com o endereço da API
+        var client = new RestClient(BASE_URL);
+        // Instancia o objeto do tipo RestRequest com o complemento de endereço
+        // como "pet" e configurando o método para ser um post (inclusão)
+        var request = new RestRequest("pet", Method.Post);
+
+        // Armazena o conteudo do arquivo pet.json na memoria
+        String jsonBody = File.ReadAllText(@"D:\ESTUDO\ITERASYS\FTS139\petstore139\fixtures\pet1.json");
+        // Adiciona na requisição o conteudo do arquivo pet1.json
+        request.AddBody(jsonBody);
+        // Executa
+        // Executa a requisição conforme a configuração realizada
+        // Guarda o json retornado no objeto response
+        var response = client.Execute(request);
+
+        // Valida
+        var responseBody = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        // Exibe o responseBody no console
+        Console.WriteLine(responseBody);
+
+        // Valide que na resposta o status code é igual ao resultado esperado (200)
+        Assert.That((int)response.StatusCode, Is.EqualTo(200));
+
+        // Valida o petId
+        //int petId = responseBody.id;
+        Assert.That(petId, Is.EqualTo(3048454));
+
+
+        // Valida o nome do animal na resposta
+        String name = responseBody.name.ToString();
+        Assert.That(name, Is.EqualTo("Lagertha"));
+        // OU
+        // Assert.That(responseBody.name.ToString(), Is.EqualTo("Lagertha"));
+
+        
+        // Valida o status do animal na resposta
+        //string status = responseBody.status;
+        Assert.That(status, Is.EqualTo("available")); 
+
+        // Armazenar os dados obtidos para usar nos proximos testes
+        Environment.SetEnvironmentVariable("petId", petId.ToString());
+
+    }
+
 
 }    
